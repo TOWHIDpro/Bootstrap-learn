3ae98527182fca4cffb0319c4535d5e9edd7c97b
TOWHIDpro <sajjad.rk05@gmail.com>
2022-06-28 17:44:32 +0600

    initial commit

---
+++ app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <navbar />
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+const title = ref('Hello World')
+
+</script>
+
+<script>
+export default {
+  head() {
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'
+        }
+      ],
+      script: [
+        {
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
+          type: 'text/javascript'
+        },
+      ]
+    }
+  }
+}
+</script>
